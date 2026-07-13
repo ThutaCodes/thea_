8f3b6bcbe87476a0f99ce1f9ff2ae8afcf7003db
ThutaCodes <thutatun533@gmail.com>
2026-07-13 21:53:58 +0630

    nxt config

--- webpage/next.config.ts
+++ webpage/next.config.ts
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: false,
-  experimental: {
-    forceSwcTransforms: false,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
